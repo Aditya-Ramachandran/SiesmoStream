f481395cd4abbaec00660a7c2948b8ce49cbee82
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-29 23:38:24 +0530

    App with just header

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import datetime as dt
+import requests
+import numpy as np
+from pandas import json_normalize
+import datetime
+import streamlit as st
+
+st.header('SiesmoStream')
+st.markdown('*SeismoStream: Because earthquakes aren\'t just for the birds.*')
+
+# st.slider('hello')
