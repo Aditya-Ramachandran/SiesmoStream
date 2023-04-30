b7498514a073525895d4ca81a6c2e814d5d9ed3e
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-30 10:14:34 +0530

    Add: Fetching data from API

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ st.header('SiesmoStream')
 st.markdown('*SeismoStream: Because earthquakes aren\'t just for the birds.*')
 
 # st.slider('hello')
+# hitting the API and storing result in response variable
+response = requests.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-04-28&endtime=2023-04-29&minmagnitude=5')
+
+# st.write(response.json())
