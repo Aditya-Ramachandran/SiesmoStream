cc7d83b1bdda063b60c950df319379a22f1be606
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-30 11:26:33 +0530

    Add: Fetch data for current day

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ st.markdown('---')
 
 today = dt.date.today()
 week_ago = today - dt.timedelta(days=7)
+yesterday = today - dt.timedelta(days=1)
 
 st.sidebar.header('SiesmoStream')
 option = st.sidebar.radio('Select', ['QuakeView', 'QuakeEdu', 'About'])
@@ -35,6 +36,10 @@ if option == 'QuakeView':
     city_date_option = st.sidebar.selectbox('Visualize On', ['Date', 'City/State'])
     if city_date_option == 'Date':
         date_option = st.sidebar.selectbox('Choose Dates', ['Today', 'This Week', 'This Month', 'This Year', 'Custom Date Range'])
+
+        if date_option == 'Today':
+            response = requests.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={}&endtime={}&minmagnitude=5'.format(yesterday, today))
+            loader_obj.load_clean_data(response)
 
 
         # String format used to put today's date and a date from week ago as params to the API
