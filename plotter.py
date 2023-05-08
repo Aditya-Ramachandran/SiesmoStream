6047b4daef9ca0a75535f5f33643d3cdd6aa13ab
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-05-08 21:24:56 +0530

    Add: File for plotting graphs

---
+++ plotter.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+
+class Plot:
+
+    def __init__(self) -> None:
+        pass
+
+    def plot_iteractive_map(self, dataframe):
+        fig = px.density_mapbox(dataframe, lat='Latitude', lon='Longitude', z='mag', radius=10,
+                        center=dict(lat=0, lon=180), zoom=0.5,
+                        mapbox_style="stamen-terrain",height=700, color_continuous_scale=px.colors.sequential.Plasma,
+                        hover_name='place')
+        st.plotly_chart(fig, use_container_width=True)
